3b6196d39f421fc6af8f3581ee09ad3ba84b86f2
joao_kalin <joao.paulo@grupoviadigital.com>
2022-04-12 21:18:45 -0300

    add .gitignore

---
+++ FandiApi/Extensoes/ExtensaoObjeto.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace FandiApi.Extensoes
+{
+    public static class ExtensaoObjeto
+    {
+        public static Dictionary<string, string> ParaDicionarioString(this object obj)
+        {
+            try
+            {
+                var jsonSerializerSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    Converters = new List<Newtonsoft.Json.JsonConverter>() { new Newtonsoft.Json.Converters.StringEnumConverter() }
+                };
+                var json = JsonConvert.SerializeObject(obj, jsonSerializerSettings);
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
